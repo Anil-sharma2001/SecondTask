c50462915dc39c1f29c358615dd258168b47b901
Anil-sharma2001 <anilatwork2001@gmail.com>
2024-06-11 17:12:45 +0530

    error removed

--- index.js
+++ index.js
@@ -16,46 +16,41 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb+srv://anilkumar2001:anil2001@userdata.wdht2zr.mongodb.net/');
-const db = mongoose.connection;
-db.on('error', () => console.log('error'));
-db.once('open', () => console.log('connected'));
+mongoose.connect('mongodb+srv://anilkumar2001:anil2001@userdata.wdht2zr.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const userSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    mobile: String,
-    email: String,
-    street: String,
-    city: String,
-    state: String,
-    country: String,
-    login: String,
-    password: String,
-    socketId: String
+  firstName: String,
+  lastName: String,
+  mobile: String,
+  email: String,
+  street: String,
+  city: String,
+  state: String,
+  country: String,
+  login: String,
+  password: String,
+  socketId: String
 });
 
 const User = mongoose.model('User', userSchema);
 
 io.on('connection', (socket) => {
-    console.log('New user connected');
+  console.log(`New client connected: ${socket.id}`);
 
-    // Join the user to the "live_users" room
-    socket.join('live_users');
+  socket.on('new_user', async (data) => {
+    const user = new User({ ...data, socketId: socket.id });
+    await user.save();
+    io.emit('user_list', await User.find());
+  });
 
-    // When a new user is added to MongoDB
-    socket.on('new_user', async (user) => {
-        user.socketId = socket.id;
-        const newUser = new User(user);
-        await newUser.save();
-        io.to('live_users').emit('user_list', await User.find({}));
-    });
+  socket.on('get_user_list', async () => {
+    const users = await User.find();
+    io.emit('user_list', users);
+  });
 
-    // Disconnect event
-    socket.on('disconnect', async () => {
-        await User.findOneAndDelete({ socketId: socket.id });
-        io.to('live_users').emit('user_list', await User.find({}));
-    });
+  socket.on('disconnect', () => {
+    console.log(`Client disconnected: ${socket.id}`);
+  });
 });
 
 app.get('/getUser', (req, res) => {
@@ -67,54 +62,28 @@ app.get('/getUser', (req, res) => {
         });
 });
 
-app.post("/sign_up", async (req, res) => {
-    const { login, password } = req.body;
-
-    try {
-        const existingUser = await User.findOne({ login });
-
-        if (existingUser) {
-            if (existingUser.password === password) {
-                res.redirect('login.html')
-            } else {
-                return res.status(400).send('User with the same login ID already exists');
-            }
-        }
-
-        const newUser = new User({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            mobile: req.body.mobile,
-            email: req.body.email,
-            street: req.body.street,
-            city: req.body.city,
-            state: req.body.state,
-            country: req.body.country,
-            login: req.body.login,
-            password: req.body.password,
-        });
-
-        await newUser.save();
-
-        // Emit the new user event
-        io.emit('new_user', newUser);
-
-        res.redirect('/');
-    } catch (error) {
-        console.error('Error occurred during submission:', error);
-        res.status(500).send('Error occurred during submission.');
-    }
+app.get('/getUser/:socketId', async (req, res) => {
+  const user = await User.findOne({ socketId: req.params.socketId });
+  res.json(user);
 });
 
-app.get('/', (req, res) => {
-    res.set("Access-Control-Allow-Origin", "*"); 
-    return res.redirect('index.html');
-});
-
-app.get('/user', (req, res) => {
-    return res.redirect('user.html');
+app.post('/sign_up', async (req, res) => {
+  const user = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    mobile: req.body.mobile,
+    email: req.body.email,
+    street: req.body.street,
+    city: req.body.city,
+    state: req.body.state,
+    country: req.body.country,
+    login: req.body.login,
+    password: req.body.password,
+  });
+  await user.save();
+  res.redirect('/');
 });
 
 server.listen(port, () => {
-    console.log('server started');
+  console.log(`Server is running on port ${port}`);
 });
